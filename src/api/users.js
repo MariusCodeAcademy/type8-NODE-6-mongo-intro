193c57d4ac5e10bef83539ca9a73eada89193487
Marius <marius@example.com>
2022-04-13 12:02:40 +0300

    mongoPost

--- src/api/users.js
+++ src/api/users.js
@@ -1,10 +1,7 @@
-require('dotenv').config();
 const express = require('express');
-const { MongoClient } = require('mongodb');
+const { dbClient } = require('../config');
 
 const usersRoutes = express.Router();
-// console.log('process.env.MONGO_DB_STRING ===', process.env.MONGO_DB_STRING);
-const dbClient = new MongoClient(process.env.MONGO_DB_STRING);
 
 usersRoutes.get('/users', async (req, res) => {
   try {
@@ -12,7 +9,36 @@ usersRoutes.get('/users', async (req, res) => {
     await dbClient.connect();
     console.log('open conn');
     // atliksim veiksma (gautis duom, irasyti duom, antnaujinti )
-    res.json('conn ok');
+    const usersArr = await dbClient.db('cao_intro').collection('users').find().toArray();
+    console.log('usersArr ===', usersArr);
+    res.json(usersArr);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json('something went wrong');
+  } finally {
+    // uzdaryti prisijungima
+    await dbClient.close();
+    console.log('close conn');
+  }
+});
+
+usersRoutes.post('/users', async (req, res) => {
+  try {
+    // prisinjungsim prie mongoDb
+    await dbClient.connect();
+    console.log('open conn');
+    // atliksim veiksma (gautis duom, irasyti duom, antnaujinti )
+    const newUser = {
+      name: 'James',
+      hasCar: true,
+      town: 'Kaunas',
+    };
+    const insertResult = await dbClient
+      .db('cao_intro')
+      .collection('users')
+      .insertOne(newUser);
+    console.log('insertResult ===', insertResult);
+    res.json(insertResult);
   } catch (error) {
     console.log(error.message);
     res.status(500).json('something went wrong');
--- src/server.js
+++ src/server.js
@@ -1,15 +1,15 @@
-require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const usersRoutes = require('./api/users');
+const { PORT } = require('./config');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
 
 // MiddleWare
 app.use(cors());
 app.use(morgan('dev'));
+app.use(express.json());
 
 // Routes
 app.use('/', usersRoutes);
