6cf5e62267e54c5e3e5824659480d47e0b0b151d
Marius <marius@example.com>
2022-04-13 11:01:55 +0300

    conn

--- src/api/users.js
+++ src/api/users.js
@@ -1,9 +1,26 @@
+require('dotenv').config();
 const express = require('express');
+const { MongoClient } = require('mongodb');
 
 const usersRoutes = express.Router();
+// console.log('process.env.MONGO_DB_STRING ===', process.env.MONGO_DB_STRING);
+const dbClient = new MongoClient(process.env.MONGO_DB_STRING);
 
-usersRoutes.get('/users', (req, res) => {
-  res.json('GET /users route');
+usersRoutes.get('/users', async (req, res) => {
+  try {
+    // prisinjungsim prie mongoDb
+    await dbClient.connect();
+    console.log('open conn');
+    // atliksim veiksma (gautis duom, irasyti duom, antnaujinti )
+    res.json('conn ok');
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json('something went wrong');
+  } finally {
+    // uzdaryti prisijungima
+    await dbClient.close();
+    console.log('close conn');
+  }
 });
 
 module.exports = usersRoutes;
--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const usersRoutes = require('./api/users');
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 // MiddleWare
 app.use(cors());
@@ -12,4 +14,9 @@ app.use(morgan('dev'));
 // Routes
 app.use('/', usersRoutes);
 
-app.listen(3000, () => console.log('server running on port'));
+// 404
+app.use((req, res) => {
+  res.status(404).json({ err: 'not found' });
+});
+
+app.listen(PORT, () => console.log('server running on port', +PORT));
