01eba6fcee52a873d188d06ab2f862bd1d29db27
Marius <marius@example.com>
2022-04-14 09:06:46 +0300

    data

---
+++ src/data/data.js
@@ -0,0 +1,35 @@
+const users = [
+  {
+    name: 'James',
+    age: 20,
+    isStudent: true,
+  },
+  {
+    name: 'Jill',
+    age: 25,
+    isStudent: true,
+    gender: 'female',
+  },
+  {
+    name: 'Tedy',
+    age: 18,
+    isStudent: false,
+    gender: 'male',
+  },
+  {
+    name: 'Marry',
+    age: 28,
+    isStudent: false,
+    gender: 'female',
+    town: 'London',
+  },
+  {
+    name: 'Jane',
+    age: 22,
+    isStudent: true,
+    gender: 'female',
+  },
+][
+  ({ name: 'Marry', age: 28, isStudent: false, gender: 'female', town: 'London' },
+  { name: 'Jane', age: 22, isStudent: true, gender: 'female' })
+];
